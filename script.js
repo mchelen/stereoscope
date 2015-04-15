6a2bce7bf4d800a1322a2122755775ee1ef5a391
Mike Chelen <michael.chelen@gmail.com>
2015-04-14 21:00:31 -0400

    basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    var video, canvas, context, imageData, detector;
+    var video, canvas, context, imageData;
   
     function onLoad(){
       video = document.getElementById("video");
@@ -25,8 +25,6 @@
         }
         
         navigator.getUserMedia({video: true}, successCallback, errorCallback);
-        
-        detector = new AR.Detector();
 
         requestAnimationFrame(tick);
       }
@@ -34,13 +32,8 @@
     
     function tick(){
       requestAnimationFrame(tick);
-      
-      if (video.readyState === video.HAVE_ENOUGH_DATA){
+           if (video.readyState === video.HAVE_ENOUGH_DATA){
         snapshot();
-
-        var markers = detector.detect(imageData);
-        drawCorners(markers);
-        drawId(markers);
       }
     }
 
@@ -49,53 +42,4 @@
       imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     }
           
-    function drawCorners(markers){
-      var corners, corner, i, j;
-    
-      context.lineWidth = 3;
-
-      for (i = 0; i !== markers.length; ++ i){
-        corners = markers[i].corners;
-        
-        context.strokeStyle = "red";
-        context.beginPath();
-        
-        for (j = 0; j !== corners.length; ++ j){
-          corner = corners[j];
-          context.moveTo(corner.x, corner.y);
-          corner = corners[(j + 1) % corners.length];
-          context.lineTo(corner.x, corner.y);
-        }
-
-        context.stroke();
-        context.closePath();
-        
-        context.strokeStyle = "green";
-        context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
-      }
-    }
-
-    function drawId(markers){
-      var corners, corner, x, y, i, j;
-      
-      context.strokeStyle = "blue";
-      context.lineWidth = 1;
-      
-      for (i = 0; i !== markers.length; ++ i){
-        corners = markers[i].corners;
-        
-        x = Infinity;
-        y = Infinity;
-        
-        for (j = 0; j !== corners.length; ++ j){
-          corner = corners[j];
-          
-          x = Math.min(x, corner.x);
-          y = Math.min(y, corner.y);
-        }
-
-        context.strokeText(markers[i].id, x, y)
-      }
-    }
-
     window.onload = onLoad;
